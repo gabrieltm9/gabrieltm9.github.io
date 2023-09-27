c5920a93d65df388845bc78b3dc2db1e1ed5be0b
Gabriel Moncau <gabriel.moncau@hotmail.com>
2023-09-27 19:00:35 +0200

    Portfolio scrolling, selection matching

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,4 @@
 const menuItems = document.querySelectorAll("#menu-list li a");
-
 var fadeSpeed = 120 //ms lower is faster
 
 menuItems.forEach((item) => {
@@ -47,3 +46,41 @@ function linkAction() {
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    var index = 0;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY < sectionTop && index == 0) {
+            document.querySelector('.menu a[href*=' + sectionId + ']').classList.add('active')
+        } else if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.menu a[href*=' + sectionId + ']').classList.add('active')
+            oneActive = true;
+        } else {
+            document.querySelector('.menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+
+        index++;
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    //     reset: true
+});
+
+sr.reveal('.section-title', {});
+sr.reveal('.work__container', {});
