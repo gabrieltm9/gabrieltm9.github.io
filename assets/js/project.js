60a9a15b310687bd139abcb7cee10225aeb7c4f5
Gabriel Moncau <gabriel.moncau@hotmail.com>
2023-09-28 16:14:40 +0200

    Project page mobile scaling, layout

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,3 +1,32 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/* OutDiv Close Menu*/
+const outdiv = document.getElementById("outdiv")
+outdiv.onclick = function () {
+    document.getElementById("nav-menu").classList.toggle('show')
+};
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
