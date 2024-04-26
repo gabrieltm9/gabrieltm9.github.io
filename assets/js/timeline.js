e6fecf52785c41a49d75ec58a115fbd0203bfd44
Gabriel Moncau <gabriel.moncau@hotmail.com>
2024-04-25 21:01:59 -0400

    Working sticky timeline

---
+++ assets/js/timeline.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const stickyTimeline = document.querySelector('.timeline-container.sticky');
+    const sectionExpand = document.querySelector('.section.expand');
+    
+    let isStickyActive = false;
+  
+    function handleScroll() {
+      const stickyTop = stickyTimeline.getBoundingClientRect().top;
+      const stickyBottom = stickyTimeline.getBoundingClientRect().bottom;
+  
+      if (stickyTop <= window.innerHeight * 0.3 && stickyBottom > 0) {
+        // Activate sticky behavior
+        if (!isStickyActive) {
+          stickyTimeline.classList.add('fixed');
+          isStickyActive = true;
+        }
+  
+        // Horizontal scroll logic
+        let scrollDistance = window.scrollY - sectionExpand.offsetTop;
+        stickyTimeline.scrollLeft = scrollDistance;
+        console.log(stickyTimeline.scrollLeft);
+      } else {
+        // Deactivate sticky behavior
+        if (isStickyActive) {
+          stickyTimeline.classList.remove('fixed');
+          isStickyActive = false;
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', handleScroll);
+  });
+  
