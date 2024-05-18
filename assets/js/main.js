4e1bb30da82388afc2190be68a79a89e991b958e
Gabriel Moncau <gabriel.moncau@hotmail.com>
2024-05-18 18:11:51 -0400

    Changed BU timeline date to include grad year

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault(); // Prevent default anchor behavior
 
         const targetId = this.getAttribute('href'); // Get the target section ID from the href attribute
+        
+        //Set browser url to the curernt url + targetId
+        window.history.pushState(null, null, window.location.href.split('#')[0] + targetId);
+
         const targetSection = document.querySelector(targetId); // Select the target section
 
         if (targetSection) {
